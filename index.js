1642a680a3aada5f0c2dde2939c3e86b4658e222
hashrock <hashrock@users.noreply.github.com>
2020-11-10 23:49:14 +0900

    chore: add menubar

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ const gui = require('gui')
 
 const win = gui.Window.create({ frame: true, transparent: false })
 const size = { width: 400, height: 400 }
+
+const menu = gui.MenuBar.create([
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        onClick: () => gui.MessageLoop.quit()
+      },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'copy' },
+      { role: 'cut' },
+      { role: 'paste' },
+      { role: 'select-all' },
+      { type: 'separator' },
+      { role: 'undo' },
+      { role: 'redo' },
+    ],
+  },
+])
+
+win.setMenuBar(menu)
+
+
 win.setContentSize(size)
 win.onClose = () => gui.MessageLoop.quit()
 
@@ -12,7 +41,7 @@ win.setContentView(contentview)
 const canvas = gui.Canvas.create(size, 1)
 
 contentview.onDraw = (self, painter) => {
-    painter.drawCanvas(canvas, size)
+  painter.drawCanvas(canvas, size)
 }
 
 let oldX = 0;
@@ -20,37 +49,37 @@ let oldY = 0;
 let down = false;
 
 contentview.onMouseDown = (self, event) => {
-    oldX = event.positionInView.x
-    oldY = event.positionInView.y
-    down = true
+  oldX = event.positionInView.x
+  oldY = event.positionInView.y
+  down = true
 }
 contentview.onMouseUp = (self, event) => {
-    down = false
+  down = false
 }
 
 contentview.onMouseMove = (self, event) => {
-    if (down) {
-        const x = event.positionInView.x
-        const y = event.positionInView.y
-        const painter = canvas.getPainter()
-        painter.setStrokeColor('#333399')
-        painter.setLineWidth(10)
-        painter.beginPath()
-        painter.moveTo({ x: oldX, y: oldY })
-        painter.lineTo({ x: x, y: y })
-        painter.stroke()
+  if (down) {
+    const x = event.positionInView.x
+    const y = event.positionInView.y
+    const painter = canvas.getPainter()
+    painter.setStrokeColor('#333399')
+    painter.setLineWidth(10)
+    painter.beginPath()
+    painter.moveTo({ x: oldX, y: oldY })
+    painter.lineTo({ x: x, y: y })
+    painter.stroke()
 
-        oldX = x;
-        oldY = y;
-        contentview.schedulePaint()
+    oldX = x;
+    oldY = y;
+    contentview.schedulePaint()
 
-    }
+  }
 }
 
 win.center()
 win.activate()
 
 if (!process.versions.yode) {
-    gui.MessageLoop.run()
-    process.exit(0)
+  gui.MessageLoop.run()
+  process.exit(0)
 }
